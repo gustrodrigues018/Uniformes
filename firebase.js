ea67ebfffeb3382464a6bf12da312653451bb045
gustrodrigues018 <f.gutavo8@gmail.com>
2026-01-05 13:01:11 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,20 @@
 <script type="module">
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_PROJETO.firebaseapp.com",
-  databaseURL: "https://SEU_PROJETO-default-rtdb.firebaseio.com",
-  projectId: "SEU_PROJETO"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD-637RqGj1lk9Bdgz5n42PdBYYyZIMH3A",
+    authDomain: "uniformes-66281.firebaseapp.com",
+    databaseURL: "https://uniformes-66281-default-rtdb.firebaseio.com",
+    projectId: "uniformes-66281",
+    storageBucket: "uniformes-66281.firebasestorage.app",
+    messagingSenderId: "263688770096",
+    appId: "1:263688770096:web:24a72f9c8eeca8998a53f5"
+  };
 
-export const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 </script>
