8db75ef633c20cc3c2a782a5dfe1cbee160f9385
gustrodrigues018 <f.gutavo8@gmail.com>
2026-01-05 11:56:22 -0300

    Add form submission for new order in pedido.js

---
+++ js/pedido.js
@@ -0,0 +1,23 @@
+import { db } from "./firebase.js";
+import { ref, push } from 
+"https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
+
+document.getElementById("formPedido").addEventListener("submit", e => {
+  e.preventDefault();
+
+  const pedido = {
+    nome: nome.value,
+    cargo: cargo.value,
+    matricula: matricula.value,
+    uniforme: uniforme.value,
+    tamanho: tamanho.value,
+    quantidade: quantidade.value,
+    observacoes: observacoes.value,
+    status: "processando",
+    dataPedido: new Date().toLocaleString()
+  };
+
+  const novoPedido = push(ref(db, "pedidos"), pedido);
+
+  window.location.href = `status.html?id=${novoPedido.key}`;
+});
